afdb8066c0da264440f47906f0ce1e0813d88fe8
khalifaSahli <79976161+khalifaSahli@users.noreply.github.com>
2022-11-11 00:02:01 +0100

    Update ProduitServiceImpl.java

--- src/main/java/com/esprit/examen/services/ProduitServiceImpl.java
+++ src/main/java/com/esprit/examen/services/ProduitServiceImpl.java
@@ -61,9 +61,9 @@ public class ProduitServiceImpl implements IProduitService {
 	@Override
 	public void assignProduitToStock(Long idProduit, Long idStock) {
 		Produit produit = produitRepository.findById(idProduit).orElse(null);
-		Stock stock = stockRepository.findById(idStock).orElse(null);
-		produit.setStock(stock);
-		produitRepository.save(produit);
+			if (produit != null) {
+			produitRepository.save(produit);
+		}
 
 	}
 
