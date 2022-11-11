9e245d38b190e36b1d29178cf2f2cb9cb9852855
khalifaSahli <79976161+khalifaSahli@users.noreply.github.com>
2022-11-11 04:20:47 +0100

    Update StockRestController.java

--- src/main/java/com/esprit/examen/controllers/StockRestController.java
+++ src/main/java/com/esprit/examen/controllers/StockRestController.java
@@ -39,7 +39,7 @@ public class StockRestController {
 	@PostMapping("/add-stock")
 	@ResponseBody
 	public Stock addStock(@RequestBody Stock s) {
-	    return s;
+	    return stockService.addStock(s);
 		 
 	}
 
@@ -54,7 +54,7 @@ public class StockRestController {
 	@PutMapping("/modify-stock")
 	@ResponseBody
 	public Stock modifyStock(@RequestBody Stock stock) {
-		return stock ;
+	         return stockService.updateStock(stock);
 	}
 
 	/*
