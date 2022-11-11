1673ea1435669fd324c46ceda5d02f6e71559c5a
khalifaSahli <79976161+khalifaSahli@users.noreply.github.com>
2022-11-11 15:00:23 +0100

    Update StockRestController.java

--- src/main/java/com/esprit/examen/controllers/StockRestController.java
+++ src/main/java/com/esprit/examen/controllers/StockRestController.java
@@ -15,6 +15,7 @@ import io.swagger.annotations.Api;
 @RestController
 @Api(tags = "Gestion des stocks")
 @RequestMapping("/stock")
+@CrossOrigin("*")
 public class StockRestController {
 
 	@Autowired
