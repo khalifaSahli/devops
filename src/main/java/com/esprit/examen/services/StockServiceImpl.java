9dbc8d5aa5ed5efac94bb8f50bca1d02e2d04034
khalifaSahli <79976161+khalifaSahli@users.noreply.github.com>
2022-11-11 01:43:22 +0100

    Update StockServiceImpl.java

--- src/main/java/com/esprit/examen/services/StockServiceImpl.java
+++ src/main/java/com/esprit/examen/services/StockServiceImpl.java
@@ -21,7 +21,7 @@ public class StockServiceImpl implements IStockService {
 	public List<Stock> retrieveAllStocks() {
 		// récuperer la date à l'instant t1
 		log.info("In method retrieveAllStocks");
-		List<Stock> stocks = (List<Stock>) stockRepository.findAll();
+		List<Stock> stocks =  stockRepository.findAll();
 		for (Stock stock : stocks) {
 			log.info(" Stock : " + stock);
 		}
@@ -67,21 +67,21 @@ public class StockServiceImpl implements IStockService {
 
 	@Override
 	public String retrieveStatusStock() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-		Date now = new Date();
-		String msgDate = sdf.format(now);
-		String finalMessage = "";
-		String newLine = System.getProperty("line.separator");
+
+		String sentence = "le stock:                                                                                                               " +
+				"                                ";
+		StringBuilder builder = new StringBuilder(sentence);
 		List<Stock> stocksEnRouge = (List<Stock>) stockRepository.retrieveStatusStock();
 		for (int i = 0; i < stocksEnRouge.size(); i++) {
-			finalMessage = newLine + finalMessage + msgDate + newLine + ": le stock "
-					+ stocksEnRouge.get(i).getLibelleStock() + " a une quantité de " + stocksEnRouge.get(i).getQte()
-					+ " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getQteMin()
-					+ newLine;
+			builder.insert(10, stocksEnRouge.get(i).getLibelleStock());
+			builder.insert(10+stocksEnRouge.get(i).getLibelleStock().length(), " a une quantité de " + stocksEnRouge.get(i).getQte());
+			builder.insert(49, " inférieur à la quantité minimale a ne pas dépasser de " + stocksEnRouge.get(i).getQte());
 
 		}
-		log.info(finalMessage);
-		return finalMessage;
+		String str = builder.toString();
+
+		log.info(str);
+		return str;
 	}
 
 }
